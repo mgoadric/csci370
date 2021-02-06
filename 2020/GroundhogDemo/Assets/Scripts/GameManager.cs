d8fd7be3ebade966ba95f03f5c607a59220a275d
Mark Goadrich <goadrich@hendrix.edu>
2021-02-06 15:50:40 -0600

    Working on the GUI and loading Scenes

--- 2020/GroundhogDemo/Assets/Scripts/GameManager.cs
+++ 2020/GroundhogDemo/Assets/Scripts/GameManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+
+    public Image spriteToFade;
+    public GameObject startButton;
+
+    public GameObject game;
 
     private void Awake()
     {
@@ -17,6 +23,7 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
     }
 
     // Start is called before the first frame update
@@ -29,5 +36,33 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void StartButton()
+    {
+        startButton.SetActive(false);
+        StartCoroutine(LerpFunction(targetColor, 3));
+        Instantiate(game);
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
+        camera.GetComponent<FollowCam>().playerSprite = player;
+
+    }
+
+    public Color targetColor = new Color(1, 1, 1, 0);
+
+
+    IEnumerator LerpFunction(Color endValue, float duration)
+    {
+        float time = 0;
+        Color startValue = spriteToFade.color;
+
+        while (time < duration)
+        {
+            spriteToFade.color = Color.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        spriteToFade.color = endValue;
     }
 }
