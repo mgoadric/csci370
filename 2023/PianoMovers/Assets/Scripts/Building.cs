e63dcd47141fbaad50f94a30d5c56b3c0211778f
Mark Goadrich <goadrich@hendrix.edu>
2023-01-20 10:28:56 -0600

    Repeatedly invoke the piano creation

--- 2023/PianoMovers/Assets/Scripts/Building.cs
+++ 2023/PianoMovers/Assets/Scripts/Building.cs
@@ -10,12 +10,16 @@ public class Building : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(piano);
+        InvokeRepeating("DropPiano", 2.0f, 2.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void DropPiano() {
+        Instantiate(piano);
     }
 }
