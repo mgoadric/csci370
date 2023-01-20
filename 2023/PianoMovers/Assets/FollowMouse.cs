d39a89090663d63ba2cf3fde8a895857b1374a42
Mark Goadrich <goadrich@hendrix.edu>
2023-01-20 10:05:21 -0600

    The net follows the mouse

---
+++ 2023/PianoMovers/Assets/FollowMouse.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(pos.x, transform.position.y);
+
+    }
+}
