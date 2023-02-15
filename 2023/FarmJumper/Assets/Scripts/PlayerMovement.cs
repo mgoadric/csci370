e0aafbe9c8b23363a2564e5cc3823719df2f8edc
Mark Goadrich <goadrich@hendrix.edu>
2023-02-15 14:08:11 -0600

    Added sprite and animation

--- 2023/FarmJumper/Assets/Scripts/PlayerMovement.cs
+++ 2023/FarmJumper/Assets/Scripts/PlayerMovement.cs
@@ -8,22 +8,38 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D body;
     public float horizontal;
 
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
     public float runSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("horizontal", horizontal);
+        if (horizontal < 0) {
+            spriteRenderer.flipX = true;
+        } else {
+            spriteRenderer.flipX = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            print("Space!");
+            body.AddForce(new Vector2(0, runSpeed * 100));
+        }
     }
 
     void FixedUpdate() {
 
-        body.velocity = new Vector2(horizontal * runSpeed, 0);
+        body.velocity = new Vector2(horizontal * runSpeed, body.velocity.y);
+
     }
 }
