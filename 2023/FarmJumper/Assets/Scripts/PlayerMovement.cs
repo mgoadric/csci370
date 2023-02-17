d6202508014b4bfbb932096008628dab590d178a
Mark Goadrich <goadrich@hendrix.edu>
2023-02-17 13:53:34 -0600

    Starting background

--- 2023/FarmJumper/Assets/Scripts/PlayerMovement.cs
+++ 2023/FarmJumper/Assets/Scripts/PlayerMovement.cs
@@ -6,24 +6,50 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody2D body;
+
+    private Animator animator;
+
+    private SpriteRenderer spriteRenderer;
+
     public float horizontal;
 
     public float runSpeed = 5f;
+
+    public bool jumping = false;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("horizontal", horizontal);
+        if (horizontal < 0) {
+            spriteRenderer.flipX = true;
+        } else {
+            spriteRenderer.flipX = false;
+        }
     }
 
     void FixedUpdate() {
 
         body.velocity = new Vector2(horizontal * runSpeed, body.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && !jumping) {
+            print("space");
+            body.AddForce(new Vector2(0, 400));
+            jumping = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision2D) {
+        if (jumping) {
+            jumping = false;
+        }
     }
 }
