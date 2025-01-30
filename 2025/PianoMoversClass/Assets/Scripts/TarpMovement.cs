cebce1117c7894688586a078a9ad895c78ff0d46
Mark Goadrich <goadrich@hendrix.edu>
2025-01-30 08:52:35 -0600

    The tarp moves!

---
+++ 2025/PianoMoversClass/Assets/Scripts/TarpMovement.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+public class TarpMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(pos.x, transform.position.y);
+        Debug.Log("" + pos.x);
+    }
+}
