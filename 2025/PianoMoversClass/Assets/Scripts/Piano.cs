63194d4f9f24282c8f9ea296d6673c98256a7153
Mark Goadrich <goadrich@hendrix.edu>
2025-02-06 08:43:44 -0600

    Added audio, fixed memory leaks

--- 2025/PianoMoversClass/Assets/Scripts/Piano.cs
+++ 2025/PianoMoversClass/Assets/Scripts/Piano.cs
@@ -4,17 +4,26 @@ public class Piano : MonoBehaviour
 {
 
     private Rigidbody2D rb2D;
+    private AudioSource audioSource;
+
+    private bool crashed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
         rb2D.AddForce(transform.right * 135);
+        crashed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!crashed && transform.position.y < -5) {
+            crashed = true;
+            audioSource.Play();
+            Destroy(gameObject, 4);
+        }
     }
 }
