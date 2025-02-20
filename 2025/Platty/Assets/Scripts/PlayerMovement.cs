2f5736378f4b26e57db53a6f37829eaf108f6857
Mark Goadrich <goadrich@hendrix.edu>
2025-02-20 06:05:32 -0600

    Jumping person with platforms

---
+++ 2025/Platty/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rigidbody2D;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    float horizontal;
+    float vertical;
+
+    public float runSpeed = 5f;
+    private bool m_Grounded;
+
+    public UnityEvent OnLandEvent;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        if (OnLandEvent == null) {
+		    OnLandEvent = new UnityEvent();
+        }
+        OnLandEvent.AddListener(Landed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (horizontal < 0) {
+            spriteRenderer.flipX = true;
+        } else {
+            spriteRenderer.flipX = false;
+        }
+        animator.SetFloat("horizontal", horizontal);
+        if (Input.GetKeyDown("space") && !animator.GetBool("jump"))
+        {
+            rigidbody2D.AddForce(Vector2.up * 2000);
+            animator.SetBool("jump",true);
+            Debug.Log("space key was pressed");
+        }
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody2D.linearVelocity = new Vector2(horizontal * runSpeed, rigidbody2D.linearVelocity.y);
+    
+		bool wasGrounded = m_Grounded;
+;
+        m_Grounded = false;
+
+		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.1f);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+                m_Grounded = true;
+				if (!wasGrounded)
+					OnLandEvent.Invoke();
+			}
+		}
+	}
+
+    public void Landed() {
+        animator.SetBool("jump", false);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        animator.SetBool("jump", false);
+    }
+}
