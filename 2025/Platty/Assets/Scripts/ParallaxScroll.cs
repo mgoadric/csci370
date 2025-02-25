0b51e0afc5798ead10284217955fb83e99df2523
Mark Goadrich <goadrich@hendrix.edu>
2025-02-25 06:07:57 -0600

    In background sorting layer

--- 2025/Platty/Assets/Scripts/ParallaxScroll.cs
+++ 2025/Platty/Assets/Scripts/ParallaxScroll.cs
@@ -34,6 +34,7 @@ public class ParallaxScroll : MonoBehaviour
             SpriteRenderer spriteRenderer = go.AddComponent<SpriteRenderer>();
             spriteRenderer.sprite = sprite;
             spriteRenderer.sortingOrder = sortOrder;
+            spriteRenderer.sortingLayerName = "Background";
             go.name = i.ToString();
             layers[i] = go.transform;
         }
